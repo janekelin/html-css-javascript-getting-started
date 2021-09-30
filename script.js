e22849abfa1ee683fc077bb96aa24ba90d5ec4e2
Elise Godskesen <iana.godskesen@f24s.com>
2021-09-30 13:30:04 +0200

    Updated the script

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
-console.dir(document.readyState)
+const orderButtonCollection = document.querySelectorAll('button[data-order]');
+
+orderButtonCollection.forEach((button) => {
+  button.addEventListener('click', createOrder);
+
+  function createOrder(e) {
+    const button = e.currentTarget;
+    const container = button.parentNode;
+
+    const order = {
+      id: button.getAttribute('data-order'),
+      title: container.querySelector('.js-title').innerText,
+      price: container.querySelector('.js-price').innerText,
+      desc: container.querySelector('.js-desc').innerText,
+    };
+
+    localStorage.setItem('order', JSON.stringify(order));
+
+    const orderUrl = window.location.href.replace('pies.html', 'order.html');
+    window.location.href = orderUrl;
+  }
+});
